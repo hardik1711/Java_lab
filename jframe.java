52699f9dc8952193b99db7da4e03ca546d5ca2f4
Hardik Sharma <60344125+hardik1711@users.noreply.github.com>
2021-03-16 12:24:48 +0530

    Create jframe.java

---
+++ jframe.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class jframe {
+
+
+
+
+    public static void main(String[] args) {
+        JFrame j=new JFrame();
+          j.setSize(400,400);
+          j.setVisible(true);
+          j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
